c00746191752ce2ff166e2f3bd390fc2f7432a26
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 17:04:15 -0600

    Simple communication between html and php.

---
+++ FormHandling/welcome.php
@@ -0,0 +1,7 @@
+<?php
+	echo "Welcome " . $_POST['fname'] . ".<br>";
+	echo "Your email is: " . $_POST['usremail'] . ".<br>";
+
+	echo "Welcome " . $_GET['fname2'] . ".<br>";
+	echo "Your email is: " . $_GET['usremail2'] . ".<br>";
+?>
