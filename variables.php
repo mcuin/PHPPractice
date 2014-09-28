b20611bbc10025397df5874ba11b727aebff860a
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 13:05:07 -0600

    Global keyword and global stack.

--- variables.php
+++ variables.php
@@ -23,6 +23,25 @@
 			echo "<p>Variables outside of function: </P>";
 			echo "Variable x is: $x <br>";
 			echo "Variable a is: $a <br>";
+
+			$b = 10;
+			
+			function myTest2() {
+				global $x, $b;
+				$b = $x + $b;
+			}
+
+			myTest2();
+			echo "$b<br>";
+
+			$c = 10;
+
+			function myTest3() {
+				$GLOBALS['c'] = $GLOBALS['x'] + $GLOBALS['c'];
+			}
+
+			myTest3();
+			echo "$c<br>";
 		?>
 	</body>
 </html>
