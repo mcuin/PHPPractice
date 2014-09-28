ad8e5016538552943417b26ea001726fe12efe5b
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 12:53:36 -0600

    Basic work with variables.

---
+++ variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Variables</title>
+	</head>
+	<body>
+		<?php
+			$x = 5;
+			$y = 6;
+			$z = $x + $y;
+			echo $z;
+		?>
+	</body>
+</html>
