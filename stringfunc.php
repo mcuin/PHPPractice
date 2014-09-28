e6484c5be6675398e4a2a4db1bc85e118d2b745a
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 13:55:44 -0600

    Small work with string functions.

---
+++ stringfunc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>String Functions</title>
+	</head>
+	<body>
+		<?php
+			echo strlen("Hello World!");
+			echo "<br>";
+			echo strpos("Hello World!", "World");
+		?>
+	</body>
+</html>
