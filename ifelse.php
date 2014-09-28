6228e1ccba3c5610ff52cfd4dac48c6ebc0276d1
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 15:05:19 -0600

    Inital if statement.

---
+++ ifelse.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Else If</title>
+	</head>
+	<body>
+		<?php
+			$t = date("H");
+			
+			if ($t < "20") {
+				echo "Have a good day!";
+			}
+		?>
+	</body>
+</html>
