34c9e6eb25fe3eed13f02dcc5ec3d0d0ffb8c7d2
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 15:09:03 -0600

    Added en elseif to have all three greetings.

--- ifelse.php
+++ ifelse.php
@@ -7,8 +7,9 @@
 	<body>
 		<?php
 			$t = date("H");
-			
-			if ($t < "20") {
+			if ($t < "10") {
+				echo "Have a good morning!";
+			} elseif ($t < "20") {
 				echo "Have a good day!";
 			} else {
 				echo "Have a good night!";
