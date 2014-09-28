7ff277dfe65784434cb2b52df571376c64283d72
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 12:59:11 -0600

    Global and local scope.

--- variables.php
+++ variables.php
@@ -10,6 +10,19 @@
 			$y = 6;
 			$z = $x + $y;
 			echo $z;
+			
+			function myTest() {
+				$a = 10;
+				echo "<p>Variables inside of function: </p>";
+				echo "Variable x is: $x <br>";
+				echo "Variable a is: $a <br>";
+			}
+
+			myTest();
+	
+			echo "<p>Variables outside of function: </P>";
+			echo "Variable x is: $x <br>";
+			echo "Variable a is: $a <br>";
 		?>
 	</body>
 </html>
