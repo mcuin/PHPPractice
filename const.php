d7680b1c3097fa8e1883ec597bb9ef05e603417a
Mykal Cuin <mykcuin@gmail.com>
2014-09-28 14:02:17 -0600

    Case sensetive and insentive constants.

---
+++ const.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Constants</title>
+	</head>
+	<body>
+		<?php
+			define("GREETING", "Welcome to W3Schools.com!");
+			echo GREETING;
+			define("GREETING2", "Welcome again to W3Schools.com!", true);
+			echo greeting2;
+		?>
+	</body>
+</html>
