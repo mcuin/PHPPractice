8c9b7b5bacf9eb111c08b8f66db1e07624d27763
Mykal Cuin <mykcuin@gmail.com>
2014-09-29 12:51:27 -0600

    Very basic file read.

---
+++ FileHandle/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP File Handeling</title>
+	</head>
+	<body>
+		<?php
+			echo readfile("webdictionary.txt");
+		?>
+	</body>
+</html>
