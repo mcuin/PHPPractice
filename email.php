2cc6685cb308d06b0f501762192d7f19820b4860
Mykal Cuin <mykcuin@gmail.com>
2014-09-30 13:09:37 -0600

    Added more security to email.:

--- email.php
+++ email.php
@@ -5,27 +5,43 @@
 		<title>PHP Email</title>
 	</head>
 	<body>
-		<h2>Email Form</h2>
 		<?php
-			if (!isset ($_POST["submit"])) {
-			?>
-				<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-					From: <input type="text" name="from"><br>
-					Subject: <input typ="text" name="subject"><br>
-					Message: <textarea rows="10" cols="40" name="message"></textarea><br>
-					<input type="submit" name ="submit" value="Submit Email">
-				</form>
-				<?php
-			} else {
-				if (isset($_POST["from"])) {
-					$from = $_POST["from"];
-					$subject = $_POST["subject"];
-					$message = $_POST["message"];
-					$message = wordwrap($message, 70);
-					mail("mykcuin@gmail.com", $subject, $message, "From: $from\n");
-					echo "Thank you for the email.";
-				}
+			function spamcheck($field) {
+  				$field=filter_var($field, FILTER_SANITIZE_EMAIL);
+  				if(filter_var($field, FILTER_VALIDATE_EMAIL)) {
+    					return TRUE;
+				} else {
+    					return FALSE;
+  				}
 			}
 		?>
+
+		<h2>Feedback Form</h2>
+		<?php
+			if (!isset($_POST["submit"])) {
+  		?>
+  				<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  					From: <input type="text" name="from"><br>
+  					Subject: <input type="text" name="subject"><br>
+  					Message: <textarea rows="10" cols="40" name="message"></textarea><br>
+  					<input type="submit" name="submit" value="Submit Feedback">
+  				</form>
+  		<?php 
+			} else {  
+  				if (isset($_POST["from"])) {
+    					$mailcheck = spamcheck($_POST["from"]);
+    					if ($mailcheck==FALSE) {
+      						echo "Invalid input";
+    					} else {
+      						$from = $_POST["from"];
+      						$subject = $_POST["subject"];
+     						$message = $_POST["message"];
+      						$message = wordwrap($message, 70);
+      						mail("mykcuin@gmail.com",$subject,$message,"From: $from\n");
+      						echo "Thank you for sending us feedback";
+    					}				
+  				}
+			}
+		?>	
 	</body>
 </html>
