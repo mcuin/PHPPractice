4caf95fc8df8294ccd80311d7cdb0811ec39fb9c
Mykal Cuin <mykcuin@gmail.com>
2014-09-30 09:43:55 -0600

    Added saving the uploaded file.

--- Upload/upload.php
+++ Upload/upload.php
@@ -27,6 +27,12 @@
 					echo "Type: " . $_FILES["file"]["type"] . "<br>";
 					echo "Size: " . ($_FILES["file"]["size"] / 1024) . "kB<br>";
 					echo "Stored in: " . $_FILES["file"]["tmp_name"];
+					if (file_exists("upload/" . $_FILES["file"]["name"])) {
+						echo $_FILES["file"]["name"] . " already exists.";
+					} else {
+						move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+						echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+					} 
 				}
 			} else {
 				echo "Invalid file. <br>";
